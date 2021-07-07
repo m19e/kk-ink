279ffd5fd7d9c986e521b92cf3a727d0d654163c
m19e <nexus.me7290@gmail.com>
2021-07-08 07:09:55 +0900

    Spinner demo

--- commands/index.tsx
+++ commands/index.tsx
@@ -1,11 +1,21 @@
 import React, { FC } from "react";
 import PropTypes from "prop-types";
-import { Text } from "ink";
+import { Text, Newline } from "ink";
+import Spinner from "ink-spinner";
 
 /// Hello world command
 const Hello: FC<{
 	name?: string;
-}> = ({ name = "Stranger" }) => <Text>Hello, {name}</Text>;
+}> = ({ name = "Stranger" }) => (
+	<Text>
+		Hello, {name}
+		<Newline />
+		<Text color="green">
+			<Spinner type="dots" />
+		</Text>
+		{" Loading"}
+	</Text>
+);
 
 Hello.propTypes = {
 	/// Name of the person to greet
