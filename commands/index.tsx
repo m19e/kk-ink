a6a9d37850310616d8bdcae2ef3f8c9d592662b8
m19e <nexus.me7290@gmail.com>
2021-07-07 19:39:17 +0900

    Test comp, props desc

--- commands/index.tsx
+++ commands/index.tsx
@@ -1,8 +1,15 @@
 import React, { FC } from "react";
+import PropTypes from "prop-types";
 import { Text } from "ink";
 
-const Hello: FC<{ name?: string }> = ({ name = "Stranger" }) => (
-	<Text>Hello, {name}</Text>
-);
+/// component desc test
+const Hello: FC<{
+	name?: string;
+}> = ({ name = "Stranger" }) => <Text>Hello, {name}</Text>;
+
+Hello.propTypes = {
+	/// props desc test
+	name: PropTypes.string,
+};
 
 export default Hello;
