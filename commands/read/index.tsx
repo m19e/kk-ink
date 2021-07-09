4969c8636203934be558df4b4f028ffa6ecf61c8
m19e <nexus.me7290@gmail.com>
2021-07-10 07:24:57 +0900

    Border demo

--- commands/read/index.tsx
+++ commands/read/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Text } from "ink";
+import { Box, Text } from "ink";
 import { extname } from "path";
 import { existsSync } from "fs";
 
@@ -22,7 +22,44 @@ const Read = ({ file }: Props) => {
 		return <Text>見つかりませんでした……</Text>;
 	}
 
-	return <Text>「{file}」を読み込みます！</Text>;
+	return (
+		<>
+			<Text>「{file}」を読み込みます！</Text>
+			<Box flexDirection="column" padding={2}>
+				<Box>
+					<Box borderStyle="single" marginRight={2}>
+						<Text>single</Text>
+					</Box>
+
+					<Box borderStyle="double" marginRight={2}>
+						<Text>double</Text>
+					</Box>
+
+					<Box borderStyle="round" marginRight={2}>
+						<Text>round</Text>
+					</Box>
+
+					<Box borderStyle="bold">
+						<Text>bold</Text>
+					</Box>
+				</Box>
+
+				<Box marginTop={1}>
+					<Box borderStyle="singleDouble" marginRight={2}>
+						<Text>singleDouble</Text>
+					</Box>
+
+					<Box borderStyle="doubleSingle" marginRight={2}>
+						<Text>doubleSingle</Text>
+					</Box>
+
+					<Box borderStyle="classic">
+						<Text>classic</Text>
+					</Box>
+				</Box>
+			</Box>
+		</>
+	);
 };
 
 Read.propTypes = {
