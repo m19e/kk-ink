67c8f0c09844fc62be8c1c3be8369c9894a17fd4
m19e <nexus.me7290@gmail.com>
2021-07-12 19:33:12 +0900

    Toggle input capture is active

--- commands/read/index.tsx
+++ commands/read/index.tsx
@@ -50,30 +50,38 @@ const Read = ({ file }: Props) => {
 
 const Robot = () => {
 	const { exit } = useApp();
+	const [isActive, setIsActive] = useState(true);
 	const [x, setX] = useState(1);
 	const [y, setY] = useState(1);
 
-	useInput((input, key) => {
-		if (input === "q" || key.return || key.escape) {
-			exit();
-		}
+	useInput(
+		(input, key) => {
+			if (input === "q" || key.return || key.escape) {
+				exit();
+			}
 
-		if (key.leftArrow) {
-			setX(Math.max(1, x - 1));
-		}
+			if (input === "t") {
+				setIsActive((prev) => !prev);
+			}
 
-		if (key.rightArrow) {
-			setX(Math.min(20, x + 1));
-		}
+			if (key.leftArrow) {
+				setX(Math.max(1, x - 1));
+			}
 
-		if (key.upArrow) {
-			setY(Math.max(1, y - 1));
-		}
+			if (key.rightArrow) {
+				setX(Math.min(20, x + 1));
+			}
 
-		if (key.downArrow) {
-			setY(Math.min(10, y + 1));
-		}
-	});
+			if (key.upArrow) {
+				setY(Math.max(1, y - 1));
+			}
+
+			if (key.downArrow) {
+				setY(Math.min(10, y + 1));
+			}
+		},
+		{ isActive }
+	);
 
 	return (
 		<Box flexDirection="column">
