b3be2e70c20a71f18e5c822a994b88a8c86360b3
m19e <nexus.me7290@gmail.com>
2021-07-16 01:17:59 +0900

    Return atom simply

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -3,21 +3,19 @@ import { Box, Text } from "ink";
 import Gradient from "ink-gradient";
 import BigText from "ink-big-text";
 
-const Logo = () => {
-	return (
-		<Box paddingX={2} alignItems="center">
-			<Box marginRight={2}>
-				<Gradient name="fruit">
-					<BigText text="kk" />
-				</Gradient>
-			</Box>
-			<Box paddingBottom={1}>
-				<Box borderStyle="singleDouble" paddingX={2}>
-					<Text bold>CLI tool for Kanji check</Text>
-				</Box>
+const Logo = () => (
+	<Box paddingX={2} alignItems="center">
+		<Box marginRight={2}>
+			<Gradient name="fruit">
+				<BigText text="kk" />
+			</Gradient>
+		</Box>
+		<Box paddingBottom={1}>
+			<Box borderStyle="singleDouble" paddingX={2}>
+				<Text bold>CLI tool for Kanji check</Text>
 			</Box>
 		</Box>
-	);
-};
+	</Box>
+);
 
 export default Logo;
