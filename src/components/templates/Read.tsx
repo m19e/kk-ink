ae225bafdb82e012e60b039fa7e4a218b4b0c068
m19e <nexus.me7290@gmail.com>
2021-07-16 02:24:27 +0900

    Add loading spinner

--- src/components/templates/Read.tsx
+++ src/components/templates/Read.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, Newline } from "ink";
+import Spinner from "ink-spinner";
 import { extname } from "path";
 import { existsSync, readFileSync } from "fs";
-
 import Logo from "../atoms/Logo";
 
 type Props = {
@@ -10,6 +10,9 @@ type Props = {
 };
 
 const Read = ({ file }: Props) => {
+	const [loading, setLoading] = useState(true);
+	const [lines, setLines] = useState<string[]>([]);
+
 	if (
 		typeof file !== "string" ||
 		(extname(file) !== ".txt" && extname(file) !== ".md")
@@ -22,15 +25,37 @@ const Read = ({ file }: Props) => {
 		return <Text>見つかりませんでした……</Text>;
 	}
 
-	const buf = readFileSync(file, "utf-8");
+	useEffect(() => {
+		const buf = readFileSync(file, "utf-8");
+		const ls = buf.split("\n");
+		setLines(ls);
+
+		setTimeout(() => {
+			setLoading(false);
+		}, 2000);
+	}, []);
 
 	return (
 		<>
-			<Logo />
-			<Text>「{file}」を読み込みます！</Text>
-			<Newline />
-			<Text>{buf}</Text>
+			{loading ? (
+				<Text>
+					<Text color="green">
+						<Spinner type="dots" />
+					</Text>
+					<Text> Loading</Text>
+				</Text>
+			) : (
+				<>
+					<Logo />
+					<Text>「{file}」を読み込みます！</Text>
+					<Newline />
+					{lines.map((l) => (
+						<Text>{l}</Text>
+					))}
+				</>
+			)}
 		</>
 	);
 };
+
 export default Read;
