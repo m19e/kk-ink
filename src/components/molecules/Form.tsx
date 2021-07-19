f6a7f695485b85f0c6ce9e3bfb0ac6823475e9af
m19e <nexus.me7290@gmail.com>
2021-07-20 06:55:51 +0900

    Inject line-data to form

--- src/components/molecules/Form.tsx
+++ src/components/molecules/Form.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FC } from "react";
 import { Box } from "ink";
 import {
 	Form,
@@ -6,101 +6,76 @@ import {
 	FormFieldString,
 	FormFieldValueRendererProps,
 	SpecificFormFieldRendererProps,
+	FormField,
+	FormSection,
 } from "../../ink/form";
 import TextInput from "ink-text-input";
 
 type CustomField = AbstractFormField<"custom", string> & { regex: RegExp };
 
-const FormDemoCustom = () => (
-	<Form
-		onSubmit={(value) => console.log(`Submitted: `, value)}
-		customManagers={[
-			{
-				type: "custom",
-				renderValue: ({
-					value,
-				}: FormFieldValueRendererProps<FormFieldString>) => <>{value}</>,
-				renderField: ({
-					value,
-					field: { regex, placeholder },
-					onChange,
-					onError,
-					onClearError,
-				}: SpecificFormFieldRendererProps<FormFieldString>) => (
-					<Box padding={1}>
-						<TextInput
-							value={value ?? ""}
-							onChange={(value) => {
-								onChange(value);
+const FormDemoCustom: FC<{
+	datas: {
+		id: number;
+		text: string;
+		targets: string[];
+	}[];
+}> = ({ datas }) => {
+	const formData: FormSection[] = datas
+		.filter((d) => d.targets.length)
+		.map((d) => {
+			return {
+				title: "" + d.id,
+				fields: d.targets.map((t) => {
+					return {
+						type: "custom",
+						name: t,
+						description: "Hiragana or Katakana",
+						regex: /^[\u3041-\u3096\u30A1-\u30FA]*$/,
+					};
+				}),
+			};
+		});
 
-								if (!value.match(regex)) {
-									onError(`Invaid value, should be Hiragana or Katakana`);
-								} else {
-									onClearError();
-								}
-							}}
-							placeholder={placeholder}
-						/>
-					</Box>
-				),
-			},
-		]}
-		form={{
-			title: "Custom Form Field Manager",
-			sections: [
+	return (
+		<Form
+			onSubmit={(value) => console.log(`Submitted: `, value)}
+			customManagers={[
 				{
-					title: "Main",
-					description:
-						"Demonstration of how custom field implementations can be used.",
-					fields: [
-						{
-							type: "custom",
-							name: "読めない漢字1",
-							description: "Hiragana or Katakana",
-							regex: /^[\u3041-\u3096\u30A1-\u30FA]*$/,
-						},
-						{
-							type: "custom",
-							name: "読めない漢字2",
-							description: "Hiragana or Katakana",
-							regex: /^[\u3041-\u3096\u30A1-\u30FA]*$/,
-						},
-						{
-							type: "custom",
-							name: "読めない漢字3",
-							description: "Hiragana or Katakana",
-							regex: /^[\u3041-\u3096\u30A1-\u30FA]*$/,
-						},
-					] as CustomField[],
+					type: "custom",
+					renderValue: ({
+						value,
+					}: FormFieldValueRendererProps<FormFieldString>) => <>{value}</>,
+					renderField: ({
+						value,
+						field: { regex, placeholder },
+						onChange,
+						onError,
+						onClearError,
+					}: SpecificFormFieldRendererProps<FormFieldString>) => (
+						<Box padding={1}>
+							<TextInput
+								value={value ?? ""}
+								onChange={(value) => {
+									onChange(value);
+
+									if (!value.match(regex)) {
+										onError(`Invaid value, should be Hiragana or Katakana`);
+									} else {
+										onClearError();
+									}
+								}}
+								placeholder={placeholder}
+							/>
+						</Box>
+					),
 				},
-				{
-					title: "Sub",
-					description:
-						"Demonstration of how custom field implementations can be used.",
-					fields: [
-						{
-							type: "custom",
-							name: "読めない漢字4",
-							description: "Hiragana or Katakana",
-							regex: /^[\u3041-\u3096\u30A1-\u30FA]*$/,
-						},
-						{
-							type: "custom",
-							name: "読めない漢字5",
-							description: "Hiragana or Katakana",
-							regex: /^[\u3041-\u3096\u30A1-\u30FA]*$/,
-						},
-						{
-							type: "custom",
-							name: "読めない漢字6",
-							description: "Hiragana or Katakana",
-							regex: /^[\u3041-\u3096\u30A1-\u30FA]*$/,
-						},
-					] as CustomField[],
-				},
-			],
-		}}
-	/>
-);
+			]}
+			form={{
+				title: "Custom Form Field Manager",
+				sections: formData,
+			}}
+		/>
+	);
+};
 
 export default FormDemoCustom;
