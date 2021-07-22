fd515f5ce3e104079018297f41cd9a66d3beac1f
m19e <nexus.me7290@gmail.com>
2021-07-23 01:45:05 +0900

    Shift read to default command

--- commands/index.tsx
+++ commands/index.tsx
@@ -1,25 +1,19 @@
-import React, { FC } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { Box, Text, Spacer } from "ink";
-import Spinner from "ink-spinner";
+import ReadCmd from "../src/components/templates/Read";
 
-/// Hello world command
-const Hello: FC<{
-	name?: string;
-}> = ({ name = "Stranger" }) => (
-	<Box>
-		<Text>Hello, {name}</Text>
-		<Spacer />
-		<Text color="green">
-			<Spinner type="dots" />
-		</Text>
-		<Text> Loading</Text>
-	</Box>
-);
-
-Hello.propTypes = {
-	/// Name of the person to greet
-	name: PropTypes.string,
+type Props = {
+	file: string;
 };
 
-export default Hello;
+/// Read file command
+const Read = ({ file }: Props) => <ReadCmd file={file} />;
+
+Read.propTypes = {
+	/// Name of target file name
+	file: PropTypes.string,
+};
+
+Read.positionalArgs = ["file"];
+
+export default Read;
