132cd666f2ed57982b93a80b68648565a87ecc40
m19e <nexus.me7290@gmail.com>
2021-07-22 21:34:08 +0900

    Add line-data type to root

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,11 @@
 import { AbstractFormField } from "../ink/form";
 
+export type LineData = {
+	id: number;
+	text: string;
+	targets: string[];
+};
+
 export type CustomField = AbstractFormField<"custom", string> & {
 	regex: RegExp;
 };
