329b98a4c3fbfa9c78193d17d4e072ab27b6d6e2
m19e <nexus.me7290@gmail.com>
2021-07-25 00:21:04 +0900

    Centering logo

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,20 +1,13 @@
 import React from "react";
-import { Box, Text } from "ink";
+import { Box } from "ink";
 import Gradient from "ink-gradient";
 import BigText from "ink-big-text";
 
 const Logo = () => (
-	<Box paddingX={2} alignItems="center">
-		<Box marginRight={2}>
-			<Gradient name="fruit">
-				<BigText text="kk" />
-			</Gradient>
-		</Box>
-		<Box paddingBottom={1}>
-			<Box borderStyle="round" paddingX={2}>
-				<Text bold>CLI tool for Kanji check</Text>
-			</Box>
-		</Box>
+	<Box justifyContent="center">
+		<Gradient name="fruit">
+			<BigText text="kk" />
+		</Gradient>
 	</Box>
 );
 
