dbf28bc988c032d7131627bb633296f57a954546
m19e <nexus.me7290@gmail.com>
2021-07-26 15:20:43 +0900

    Demo handling focus

---
+++ commands/demo.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { Box, Text, useInput, useFocusManager, useFocus } from "ink";
+
+/// Focus demo command
+const Focus = () => {
+	const [input, setInput] = useState("initial");
+	const fm = useFocusManager();
+
+	useInput((_, key) => {
+		if (key.rightArrow) {
+			fm.focusNext();
+			setInput("→");
+		} else if (key.leftArrow) {
+			fm.focusPrevious();
+			setInput("←");
+		} else if (key.tab) {
+			setInput((key.shift ? "shift + " : "") + "tab");
+		}
+	});
+
+	return (
+		<Box flexDirection="column" padding={1}>
+			<Box marginBottom={1}>
+				<Text>
+					Press Tab to focus next element, Shift+Tab to focus previous element,
+					Esc to reset focus.
+				</Text>
+			</Box>
+			<Box>
+				<Item label="First" />
+				<Item label="Second" />
+				<Item label="Third" />
+				<Box padding={2}>
+					<Text>{input}</Text>
+				</Box>
+			</Box>
+		</Box>
+	);
+};
+
+const Item = ({ label }: { label: string }) => {
+	const { isFocused } = useFocus();
+	return (
+		<Box
+			paddingX={3}
+			paddingY={1}
+			justifyContent="center"
+			borderStyle="round"
+			borderColor={isFocused ? "green" : undefined}
+		>
+			<Text>{label}</Text>
+		</Box>
+	);
+};
+
+export default Focus;
