310009c3473502fc7621659ac1f4345d44cc2f62
m19e <nexus.me7290@gmail.com>
2021-07-28 08:31:53 +0900

    Rename fc

--- src/components/organisms/Confirm.tsx
+++ src/components/organisms/Confirm.tsx
@@ -23,7 +23,7 @@ const Confirm = () => {
 		);
 
 	return (
-		<Focus
+		<ConfirmWindow
 			onSubmit={handleSubmit}
 			data={{
 				talker: "KK",
@@ -53,96 +53,94 @@ type ConfirmData = {
 	latter: ConfirmDataItem;
 };
 
-const Focus: FC<{ onSubmit: (id: string) => void; data: ConfirmData }> = ({
-	onSubmit,
-	data,
-}) => {
-	const [focus, setFocus] = useState<undefined | string>(undefined);
-	const [canSubmit, setCanSubmit] = useState(false);
-	const { focusNext, focusPrevious } = useFocusManager();
-	const [width, setWidth] = useState(0);
-	const [height, setHeight] = useState(0);
-	const [w, h] = useStdoutDimensions();
+const ConfirmWindow: FC<{ onSubmit: (id: string) => void; data: ConfirmData }> =
+	({ onSubmit, data }) => {
+		const [focus, setFocus] = useState<undefined | string>(undefined);
+		const [canSubmit, setCanSubmit] = useState(false);
+		const { focusNext, focusPrevious } = useFocusManager();
+		const [width, setWidth] = useState(0);
+		const [height, setHeight] = useState(0);
+		const [w, h] = useStdoutDimensions();
 
-	useEffect(() => {
-		if (w < h * 4) {
-			setWidth(Math.min(80, w));
-			setHeight(Math.min(h, 20, w / 4));
-		} else {
-			setWidth(Math.min(w, 80, h * 4));
-			setHeight(Math.min(h, 20));
-		}
-	}, [w, h]);
+		useEffect(() => {
+			if (w < h * 4) {
+				setWidth(Math.min(80, w));
+				setHeight(Math.min(h, 20, w / 4));
+			} else {
+				setWidth(Math.min(w, 80, h * 4));
+				setHeight(Math.min(h, 20));
+			}
+		}, [w, h]);
 
-	useInput((_, key) => {
-		if (key.rightArrow) {
-			focusNext();
-			setCanSubmit(true);
-		} else if (key.leftArrow) {
-			focusPrevious();
-			setCanSubmit(true);
-		} else if (key.escape) {
-			setCanSubmit(false);
-		} else if (key.return) {
-			if (canSubmit) onSubmit(focus);
-		}
-	});
+		useInput((_, key) => {
+			if (key.rightArrow) {
+				focusNext();
+				setCanSubmit(true);
+			} else if (key.leftArrow) {
+				focusPrevious();
+				setCanSubmit(true);
+			} else if (key.escape) {
+				setCanSubmit(false);
+			} else if (key.return) {
+				if (canSubmit) onSubmit(focus);
+			}
+		});
 
-	const handleFocus = (id: string) => {
-		setFocus(id);
-	};
+		const handleFocus = (id: string) => {
+			setFocus(id);
+		};
 
-	if (width === 0 || height === 0) return null;
+		if (width === 0 || height === 0) return null;
 
-	return (
-		<Box
-			flexDirection="column"
-			width={width}
-			minHeight={height}
-			borderStyle="bold"
-			borderColor="white"
-		>
-			<Box flexGrow={1} justifyContent="center" alignItems="center">
-				<Box width="90%" justifyContent="space-between">
-					<Item item={data.former} small={width < 70} onFocus={handleFocus} />
-					<Item item={data.latter} small={width < 70} onFocus={handleFocus} />
+		return (
+			<Box
+				flexDirection="column"
+				width={width}
+				minHeight={height}
+				borderStyle="bold"
+				borderColor="white"
+			>
+				<Box flexGrow={1} justifyContent="center" alignItems="center">
+					<Box width="90%" justifyContent="space-between">
+						<Item item={data.former} small={width < 70} onFocus={handleFocus} />
+						<Item item={data.latter} small={width < 70} onFocus={handleFocus} />
+					</Box>
 				</Box>
-			</Box>
-			<Box justifyContent="center">
-				<Box width="70%" flexDirection="column">
-					<Box>
+				<Box justifyContent="center">
+					<Box width="70%" flexDirection="column">
+						<Box>
+							<Box
+								minHeight={3}
+								borderStyle="round"
+								borderColor="white"
+								paddingX={3}
+								marginBottom={-1}
+							>
+								<Text>{data.talker}</Text>
+							</Box>
+						</Box>
 						<Box
-							minHeight={3}
+							height={width < 70 ? 4 : 5}
+							flexDirection="column"
 							borderStyle="round"
 							borderColor="white"
-							paddingX={3}
-							marginBottom={-1}
+							paddingX={2}
 						>
-							<Text>{data.talker}</Text>
+							{typeof data.message === "string" ? (
+								<Text>{data.message}</Text>
+							) : (
+								<>
+									{data.message.map((m, i) => (
+										<Text key={i}>{m}</Text>
+									))}
+								</>
+							)}
 						</Box>
-					</Box>
-					<Box
-						height={width < 70 ? 4 : 5}
-						flexDirection="column"
-						borderStyle="round"
-						borderColor="white"
-						paddingX={2}
-					>
-						{typeof data.message === "string" ? (
-							<Text>{data.message}</Text>
-						) : (
-							<>
-								{data.message.map((m, i) => (
-									<Text key={i}>{m}</Text>
-								))}
-							</>
-						)}
 					</Box>
 				</Box>
 			</Box>
-		</Box>
-	);
-};
+		);
+	};
 
 const Item: FC<{
 	item: ConfirmDataItem;
