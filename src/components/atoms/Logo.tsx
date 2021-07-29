0c86e3bc9a6baba663f35ff048b6be89a116fd08
m19e <nexus.me7290@gmail.com>
2021-07-29 20:07:10 +0900

    Centering logo

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Box } from "ink";
 import Gradient from "ink-gradient";
 import BigText from "ink-big-text";
 
 const Logo = () => (
-	<Gradient name="fruit">
-		<BigText text="kk" />
-	</Gradient>
+	<Box width="100%" justifyContent="center">
+		<Gradient name="fruit">
+			<BigText text="kk" />
+		</Gradient>
+	</Box>
 );
 
 export default Logo;
